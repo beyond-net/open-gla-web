561f0219d594dfd7b1c3d28261c9a77e6689e6ac
ICKelin <yingjiu.hulu@gmail.com>
2024-08-21 12:15:59 +0800

    feat: show result

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,80 @@
 <template>
-  <el-card style="width: 680px; padding: 20px">
-    <template #header>
-      <div class="card-header">
-        <h2>免费使用申请</h2>
+  <div v-if="tab===1">
+    <el-card style="width: 680px; padding: 20px">
+      <template #header>
+        <div class="card-header">
+          <h2>免费使用申请</h2>
+        </div>
+      </template>
+      <div class="container">
+        <el-form ref="formRef" :model="model" label-position="top" :rules="rules">
+          <el-form-item label="协议" prop="protocol" required>
+            <el-select v-model:model-value="model.protocol">
+              <el-option label="tcp" value="tcp" />
+            </el-select>
+          </el-form-item>
+
+          <el-form-item label="内网IP" prop="ip" required>
+            <el-input v-model:model-value="model.ip" />
+          </el-form-item>
+
+          <el-form-item label="内网端口" prop="port" required>
+            <el-input v-model.number="model.port" />
+          </el-form-item>
+
+          <el-form-item>
+            <el-button type="primary" @click="handleSubmit">提交</el-button>
+          </el-form-item>
+        </el-form>
       </div>
-    </template>
-    <div class="container">
-      <el-form ref="formRef" :model="model" label-position="top" :rules="rules">
-        <el-form-item label="协议" prop="protocol" required>
-          <el-select v-model:model-value="model.protocol">
-            <el-option label="tcp" value="tcp" />
-          </el-select>
-        </el-form-item>
+      <template #footer>
+        <span style="color: red"
+          ><b>每日免费开放1000个测试名额，先到先得</b></span
+        >
+      </template>
+    </el-card>
+  </div>
 
-        <el-form-item label="内网IP" prop="ip" required>
-          <el-input v-model:model-value="model.ip" />
-        </el-form-item>
+  <div v-if="tab===2">
+    <el-card style="width: 800px; padding: 20px">
+      <template #header>
+        <div class="card-header">
+          <h2>申请成功，以下为详细信息</h2>
+        </div>
+      </template>
+      <el-descriptions     
+      direction="vertical"
+      :column="2"
+      border>
+        <el-descriptions-item label="客户端ID">{{ result?.uuid }}</el-descriptions-item>
+        <el-descriptions-item label="协议">{{ result?.protocol }}</el-descriptions-item>
+        <el-descriptions-item label="公网IP/域名">{{ result?.public_ip }}</el-descriptions-item>
+        <el-descriptions-item label="公网端口">{{ result?.public_port }}</el-descriptions-item>
+        <el-descriptions-item label="内网IP/域名"> {{ result?.ip }}</el-descriptions-item>
+        <el-descriptions-item label="内网端口">{{ result?.port }}</el-descriptions-item>
+        <el-descriptions-item label="运行命令">
+          <p  style="line-height: 20px;">您可以先到 <a href="https://github.com/ICKelin/zta/releases/tag/v0.0.1-mvp" target="_blank">github</a>
+          下载对应的客户端文件，然后执行./zta-client_操作系统_CPU架构 -client_id={{ result?.uuid }} -server_addr=zta.beyondnetwork.net:12359
+          </p>
+          <br>
+          mac示例:<p style="line-height: 20px;"> ./zta-client_darwin_amd64 -client_id={{ result?.uuid }} -server_addr=zta.beyondnetwork.net:12359</p>
+        </el-descriptions-item>
+      </el-descriptions>
 
-        <el-form-item label="内网端口" prop="port" required>
-          <el-input v-model.number="model.port" />
-        </el-form-item>
-
-        <el-form-item>
-          <el-button type="primary" @click="handleSubmit">提交</el-button>
-        </el-form-item>
-      </el-form>
-    </div>
-    <template #footer>
-      <span style="color: red"
-        ><b>每日免费开放1000个测试名额，先到先得</b></span
-      >
-    </template>
-  </el-card>
+      <template #footer>
+        <span style="color: red"
+          ><b>申请成功后配置预计在1分钟以内生效</b></span
+        >
+      </template>
+    </el-card>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import type {FormInstance, FormRules} from "element-plus";
 import {reactive, ref} from "vue";
 
+var tab = ref<number>(1);
 interface FormModel {
   protocol: "tcp";
   ip: string;
@@ -150,9 +187,7 @@ const submit = async (protocol: string, ip: string, port: number) => {
 
   const data: ResponseModel = await response.json();
   result.value = data;
-  console.log(data);
-  console.log("访问IP: " + data.public_ip);
-  console.log("访问端口: " + data.public_port);
+  tab.value = 2;
 };
 </script>
 
